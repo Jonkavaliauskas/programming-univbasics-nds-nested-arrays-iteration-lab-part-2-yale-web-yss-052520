2c4c19263c6a8d3edc0fc8d34bdbf349a0f8fd12
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-19 08:43:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,21 +4,21 @@ def find_min_in_nested_arrays(src)
 
  outer_results = []
   row_index = 0
-  while row_index < spice_rack.count do
+  while row_index < src.count do
     element_index = 0
-    longest_string_element = ""
+    smallest_number = 0
     while element_index < spice_rack[row_index].count do
    
   
-      if spice_rack[row_index][element_index].length > longest_string_element.length
-        longest_string_element = spice_rack[row_index][element_index]
+      if src[row_index][element_index] < smallest_number
+        smallest_number = src[row_index][element_index]
       end
       element_index += 1
     end
    
-    outer_results << longest_string_element
+    outer_results << smallest_number
     row_index += 1
   end
    
-  outer_results # => ["Sporty", "Fajita Mix", "Rosemary"]
+  outer_results 
 end
